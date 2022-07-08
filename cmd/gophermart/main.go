44077af616232f27bb1076e379c0db6a4b307e66
GoSeoTaxi <95137583+GoSeoTaxi@users.noreply.github.com>
2022-07-08 12:46:45 +0300

    v0.0.1.04

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"github.com/GoSeoTaxi/yandex_go_05_p2/internal/httpserver"
@@ -20,14 +21,28 @@ func main() {
 	var fileStoragePath string
 	var dbStringConnect string
 
-	flag.StringVar(&dbStringConnect, "d", "user=postgres password=qwerty dbname=goyp sslmode=disable", "DATABASE_DSN")
-	flag.StringVar(&serverAddress, "a", "localhost:8080", "SERVER_ADDRESS")
-	flag.StringVar(&baseURL, "b", ":8080", "BASE_URL")
+	flag.StringVar(&dbStringConnect, "d", "", "DATABASE_DSN")
+	flag.StringVar(&serverAddress, "a", "", "SERVER_ADDRESS")
+	flag.StringVar(&baseURL, "b", "", "BASE_URL")
 	flag.StringVar(&fileStoragePath, "f", "", "FILE_STORAGE_PATH")
 	flag.Parse()
 
+	fmt.Println(serverAddress)
+	fmt.Println(baseURL)
+	fmt.Println(fileStoragePath)
+	fmt.Println(dbStringConnect)
+
 	fmt.Println(`+++++++++++++`)
 
+	var DB1 *sql.DB
+	var err error
+	DB1, err = sql.Open("postgres", dbStringConnect)
+	if err != nil {
+		fmt.Println(`StaticTest - err`)
+	}
+	fmt.Println(DB1)
+
+	fmt.Println(`+++++++++++++`)
 	httpserver.MainServer()
 
 }
