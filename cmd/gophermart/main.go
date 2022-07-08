2742057dc05c5a005c60a5d8b8e708fc47e1f82d
GoSeoTaxi <95137583+GoSeoTaxi@users.noreply.github.com>
2022-07-08 12:36:11 +0300

    v0.0.1.03

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/GoSeoTaxi/yandex_go_05_p2/internal/httpserver"
-	"github.com/joho/godotenv"
 	"os"
 )
 
@@ -15,9 +15,16 @@ func main() {
 	fmt.Println(vars)
 	fmt.Println(`+++++++++++++2`)
 
-	if err := godotenv.Load(); err != nil {
-		fmt.Println("No .env file found")
-	}
+	var serverAddress string
+	var baseURL string
+	var fileStoragePath string
+	var dbStringConnect string
+
+	flag.StringVar(&dbStringConnect, "d", "user=postgres password=qwerty dbname=goyp sslmode=disable", "DATABASE_DSN")
+	flag.StringVar(&serverAddress, "a", "localhost:8080", "SERVER_ADDRESS")
+	flag.StringVar(&baseURL, "b", ":8080", "BASE_URL")
+	flag.StringVar(&fileStoragePath, "f", "", "FILE_STORAGE_PATH")
+	flag.Parse()
 
 	fmt.Println(`+++++++++++++`)
 
